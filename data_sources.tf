13043840101d7e67b2c26256c9cc1d4a1cbb966d
terraform-runner <terraform-runner.oracle.com>
2025-08-01 17:23:31 +0000

    Automated commit from pipeline [skip ci]

--- data_sources.tf
+++ data_sources.tf
@@ -6,13 +6,13 @@
 #---------------------------------------------------------------------------
 #-- Looking for the CreatedBy tag in Oracle default tag namespace
 data "oci_identity_tag" "default_created_by" {
-      count            = length(data.oci_identity_tag_namespaces.oracle_default.tag_namespaces) > 0 ? 1 : 0
-    tag_name         = local.cis_created_by_tag_name
+  count            = length(data.oci_identity_tag_namespaces.oracle_default.tag_namespaces) > 0 ? 1 : 0
+  tag_name         = local.cis_created_by_tag_name
   tag_namespace_id = data.oci_identity_tag_namespaces.oracle_default.tag_namespaces[0].id
 }
 
 #-- Looking for the CreatedOn tag in Oracle default tag namespace
-  data "oci_identity_tag" "default_created_on" {
+data "oci_identity_tag" "default_created_on" {
   count            = length(data.oci_identity_tag_namespaces.oracle_default.tag_namespaces) > 0 ? 1 : 0
   tag_name         = local.cis_created_on_tag_name
   tag_namespace_id = data.oci_identity_tag_namespaces.oracle_default.tag_namespaces[0].id
